e00348f52d65d1795eba780a8c169ed742d6e563
APS-CS <qhapaq@hotmail.com>
2024-11-20 10:58:46 +0100

    Javascript klar för idag

---
+++ lektion1/script.js
@@ -0,0 +1,116 @@
+// frontend
+// allting som händer i webbläsaren
+
+
+// backend
+// allting som händer på webbservern
+
+
+// var används inte, finns kvar i gammal kod
+// var name = "Bobbo";
+
+// // let kan ändras
+// let age = 37;
+
+// // const är konstant
+// const PI = 3.14;
+
+// let text="Hello world";
+// let tal = 42;
+// let istrue = true;
+// let isfalse = false;
+
+// let person = {
+//    namne: "Bobbo",
+//    age: "37", 
+// }
+
+// let fruits = ["Apple", "Cherry"]
+
+// let sum = 5+3;
+
+// 5 == "5"; //blir true, kollar om det är samma värde
+// 5 === "5"; //blir false, kollar samma värde och samma typ, vilket blir false pga int vs string
+
+// let x = 10;
+// x += 5;
+
+// let age = 25
+
+// if (age >= 18) {
+//     console.log("Du är myndig")
+// } else{
+//     console.log("Du är inte myndig")
+// }
+
+// for (let i = 0; i < 5; i++) {
+//     console.log(i)
+// }
+
+// let i = 0
+// while (i < 5) {
+//     console.log(i);
+//     i++
+// }
+
+// let i = 0 ;
+// do {
+//     console.log(i)
+//     i++;
+// } while(i < 5);
+
+// let fruits = ["Apple", "Banana", "Cherry"]
+// for (let fruit of fruits) {
+//     console.log(fruit)
+// }
+
+// let person = {
+//     namn: "Bobbo";
+//     age: 37
+// };
+
+// for (let key in person) {
+//     console.log(key + ":" + person[key])
+// }
+
+
+// function add(a, b){
+//     return a+b;
+// }
+
+// let result = add(5,3)
+// console.log(result)
+
+
+// let add = (a,b) => {
+//     return a+b;
+// }
+
+// Nu kör vi med hemsidan och java
+
+// ska targeta i html
+// let element = document.getElementById("username");
+
+// let elementarray = document.getElementsByClassName("red")
+
+
+// let element = document.querySelector("#username")
+
+// let elementarray = document.querySelectorAll("div > p")
+
+
+// ändra efter tex ifyllt formulär
+
+// let element = document.getElementById("change");
+
+// element.innerHTML = "<p>Nu ser det ut såhär</p>" //.innerHTMl letar vi efter när haxx
+
+// element.textContent = "<p>Nu ser det ut såhär</p>" 
+
+
+// let element = document.getElementById("change");
+// let button = document.getElementById("button");
+
+// button.addEventListener("click", function (event){
+//     element.textContent = "Din data är skickad"
+// } ) ;
